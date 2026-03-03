23bae9af75cece4c02bd9df4c705141cf01f97db
Chris Franks <franks.chris@gmail.com>
2026-03-03 13:18:28 +0000

    Added Csv to object converion hander & unit tests.

---
+++ F3R4L.DevPack.EveIntel.Logger.Tests/CsvHandler/DeserializeAsync.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestArticles = F3R4L.DevPack.EveIntel.Logger;
+
+namespace F3R4L.DevPack.EveIntel.Logger.Tests.CsvHandler
+{
+    public class DeserializeAsync
+    {
+        private TestArticles.CsvHandler _objectUnderTest;
+
+        [SetUp]
+        public void Setup()
+        {
+            _objectUnderTest = new TestArticles.CsvHandler();
+        }
+
+        [Test]
+        public async Task Returns_EmptyArray_WhenGivenEmptyContext()
+        {
+            // Arrange
+            string[] context = Array.Empty<string>();
+
+            // Act
+            var result = await _objectUnderTest.DeserializeAsync<object>(context);
+
+            // Assert
+            result.Count().Should().Be(0);
+        }
+
+        [Test]
+        public async Task Returns_Properly_When_GivenValidContext()
+        {
+            // Arrange
+            string[] context = new[] { "Column1,Column2", "Value1,Value2", "Value3,Value4" };
+
+            // Act
+            var result = await _objectUnderTest.DeserializeAsync<TestObject>(context);
+
+            // Assert
+            result.Count().Should().Be(2);
+            result.First().Column1.Should().Be("Value1");
+            result.First().Column2.Should().Be("Value2");
+            result.Last().Column1.Should().Be("Value3");
+            result.Last().Column2.Should().Be("Value4");
+        }
+
+        private class TestObject
+        {
+            public string Column1 { get; set; }
+            public string Column2 { get; set; }
+        }
+    }
+}
---
+++ F3R4L.DevPack.EveIntel.Logger/CsvHandler.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F3R4L.DevPack.EveIntel.Logger
+{
+    public class CsvHandler
+    {
+        public Task<T[]> DeserializeAsync<T>(string[] context)
+        {
+            if (context == null || context.Count() == 0)
+            {
+                return Task.FromResult(Array.Empty<T>());
+            }
+            return Task.Run(async () =>
+            {
+                var csvHelper = new TextFieldParser(new StringReader(string.Join(Environment.NewLine, context)))
+                {
+                    TextFieldType = FieldType.Delimited,
+                    Delimiters = new[] { "," }
+                };
+                var fieldNames = csvHelper.ReadFields();
+
+                if (!fieldNames.All(f => typeof(T).GetProperties().Select(s => s.Name).Contains(f)))
+                {
+                    throw new InvalidOperationException("CSV field names do not match the properties of the target type.");
+                }
+
+                var results = new List<T>();
+
+                while (!csvHelper.EndOfData)
+                {
+                    var fields = csvHelper.ReadFields();
+                    var obj = Activator.CreateInstance<T>();
+                    for (int i = 0; i < fieldNames.Length; i++)
+                    {
+                        var property = typeof(T).GetProperty(fieldNames[i]);
+                        if (property != null)
+                        {
+                            var value = Convert.ChangeType(fields[i], property.PropertyType);
+                            property.SetValue(obj, value);
+                        }
+                    }
+                    results.Add(obj);
+                }
+
+                return results.ToArray();
+            });
+        }
+    }
+}
